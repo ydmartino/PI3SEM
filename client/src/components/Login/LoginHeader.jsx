e7c65285910e946af6d1e686a56a8857e73a465f
Yggdrae <victorcnicacio@hotmail.com>
2024-10-24 21:47:35 -0300

    Pag de Login Componentizada. Closes #8

---
+++ client/src/components/Login/LoginHeader.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function LoginHeader({ toggleForm }) {
+
+    return (
+        <div className="loginHeader">
+                    <p>Login</p>
+                    <label className='switch'>
+                        <input type="checkbox" />
+                        <span className='slider round' onClick={toggleForm}></span>
+                    </label>
+                    <p>Cadastro</p>
+                </div>
+    )
+}
+
+export default LoginHeader
