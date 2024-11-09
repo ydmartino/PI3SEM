49922a855b1e555d738d12331390c98d287ddec2
RenanMMz <renan.m.mlol@gmail.com>
2024-11-09 17:45:35 -0300

    Fixed Cors implementation for Contact

--- server/quepassa-api/src/main/java/com/quepassa/crm/controller/ContactController.java
+++ server/quepassa-api/src/main/java/com/quepassa/crm/controller/ContactController.java
@@ -3,6 +3,7 @@ package com.quepassa.crm.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,14 +19,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/Contacts")
 public class ContactController {
     
+
+
     @Autowired
     private ContactsRepository contactsRepository;
     
+    @CrossOrigin(origins = "http://localhost:5173")
     @GetMapping
-    public Contacts greetingWithJavaConfig(@RequestParam(required = false, defaultValue = "World") String name) {
+    public Contacts contactTeste(@RequestParam(required = false, defaultValue = "World") String name) {
 		System.out.println("Ola Mundo!");
         return new Contacts();
 	}
+
+    @GetMapping("/Contact-javaconfig")
+    public Contacts contactTesteJavaconfig(@RequestParam(required = false, defaultValue = "World") String param) {
+        return new Contacts();
+    }
+    
 
     
     /*@GetMapping
