432f8f7d43ab0792daecdbd71080348555ac52a1
RenanMMz <renan.m.mlol@gmail.com>
2024-11-22 00:20:02 -0300

    Added "Save Message" function to /SendMessage (whoops)

--- server/quepassa-api/src/main/java/com/quepassa/crm/WebSocketConfig.java
+++ server/quepassa-api/src/main/java/com/quepassa/crm/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS(); // Endpoint WebSocket
+        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:5173"); // Endpoint WebSocket
     }
 
 
